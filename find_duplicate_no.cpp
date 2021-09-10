e9f29f0aafb30a7f6457950eb687f882559fcfb7
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-09-10 11:58:47 +0530

    Update find_duplicate_no.cpp

--- find_duplicate_no.cpp
+++ find_duplicate_no.cpp
@@ -37,20 +37,40 @@ int main()
     return 0;
 }
 
+// OPTIMAL - 
 
+#include <iostream>
+using namespace std;
 
+/*
+Algo:
+1. Initialize 2 pointers - Fast and slow pointers and point them to first element of array.
+2. The slow ptr moves by 1 step, while the fast ptr moves by 2 steps.
+3. Run a while loop until the fast and slow pointers become equal and stop at that point.
+4. Now place the fast ptr at the start of array.
+5. Now move both the pointers simultaneously by 1, and the point where they collide will be your duplicate number.
+*/
 
+int main()
+{
+    int nums[] = {1,3,4,2,2};
+    
+    int slow = nums[0];
+    int fast = nums[0];
+    
+    do{
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    }while(slow != fast);
+    
+    fast = nums[0];
+    
+    while(slow != fast){
+        slow = nums[slow];
+        fast = nums[fast];
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    cout<<slow;
+    return 0;
+}
 
