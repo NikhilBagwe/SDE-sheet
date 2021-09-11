c34649349064e79d44c3ba9c5a245bccff32ac0b
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-09-11 17:21:03 +0530

    1

--- Merge Intervals.cpp
+++ Merge Intervals.cpp
@@ -1,3 +1,57 @@
+// BRUTE - For each interval , linearly traverse the whole intervals vector and merge intervals that meet the condition ============ O(n^2) =====================
+
+vector<vector<int>> merge(vector<vector<int>> &intervals){
+    vector<vector<int>> mergedIntervals;
+    
+    if(intervals.size() == 0){
+        return mergedIntervals;
+    }
+    
+    sort(intervals.begin(), intervals.end());
+    cout<<"Sorted intervals : ";
+    for(auto it : intervals){
+        cout<<"["<<it[0]<<","<<it[1]<<"]"<<" ";
+    }
+    cout<<endl;
+   
+    // Assign a temporary vector
+    vector<int> temp={0,0}; 
+   
+    // linearly traverse the intervals 2D vector, for each interval
+    for(auto it: intervals){
+        // if the 1st element of current 'it' is smaller than the current 'temp', it means the interval pointed by 'it' is already merged so skip it
+        // eg: temp=[1,6]  it=[2,4] -> here [2,4] is already considered so skip it.
+        if(it[0] < temp[1]){
+            continue;
+        }
+        
+        else{
+            temp = it;
+            // consider temp=[8,9] , now iterate the intervals vector to find intervals that can be merged with it
+            for(auto it : intervals){
+               if(it[0] < temp[1]){
+                  temp[1] = max(it[1], temp[1]);
+               }
+            }
+            mergedIntervals.push_back(temp);
+         }
+     }
+    
+    for(auto it : mergedIntervals){
+        cout<<"["<<it[0]<<","<<it[1]<<"]"<<" ";
+    }
+}
+
+int main()
+{
+    vector<vector<int>> intervals{{1, 3}, {2, 6}, {8, 10}, {8, 9}, {9, 11}, {15, 18}, {2, 4}, {16, 17}};
+    merge(intervals);
+
+    return 0;
+}
+
+
+
 // OPTIMAL =========================== TC = O(nlogn) + O(n) [sorting + linear traversal] =============================================
 
 #include <bits/stdc++.h>
