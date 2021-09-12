08c2905d182beda00f0f2ae4679f1b25887f3d92
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-09-12 18:09:53 +0530

    Update Count pairs with given Sum.cpp

--- Count pairs with given Sum.cpp
+++ Count pairs with given Sum.cpp
@@ -21,6 +21,39 @@ int main()
     return 0;
 }
 
-// OPTIMAL =============================================================================
+// BETTER ============================================================== TWO POINTER APPROACH  ===============================================================
+
+int main()
+{
+    int a[] = {1, 5, 7, 1};
+    int n = 4, target = 6;
+    
+    int count = 0, sum=0;
+    int i=0, j=n-1;
+    
+    // sort the array
+    sort(a, a+n); // 1 1 5 7
+    
+    while(i < j){
+        sum = a[i] + a[j];
+        
+        if(sum == target){
+            count++;
+            i++;
+        }
+        
+        else if(sum > target){
+            j--;
+        }
+        
+        else if(sum < target){
+            i++;
+        }
+    }
+    
+    cout<<count;
+
+    return 0;
+}
 
 
