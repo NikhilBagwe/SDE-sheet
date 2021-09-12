1969ba053b3ddfbacec7ac9bfc528b5f3e5ab230
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-09-12 19:08:54 +0530

    Update Count pairs with given Sum.cpp

--- Count pairs with given Sum.cpp
+++ Count pairs with given Sum.cpp
@@ -56,4 +56,35 @@ int main()
     return 0;
 }
 
+// OPTIMAL ========================================================= O(n) =====================================================
 
+int main()
+{
+    int a[] = {1, 5, 7, 1};
+    int n = 4, target = 6;
+    
+    int count = 0;
+    
+    unordered_map<int, int> map;
+    for(int i=0; i<n; i++){
+        // find the diff betn the current elementt and target
+        int x = target - a[i]; // 5 1 -1 5
+        
+        // if x is not stored in the map, than store it and increase its frequency by 1
+        if(map[x] == 0){
+            map[a[i]]++;
+        }
+        
+        // if x is present in the map, then just add x's frequency to counter 
+        else{
+            count += map[x];
+            // also add the current element in the map
+            map[a[i]]++;
+        }
+    }
+    
+    // count=2, as there are two 1's in arr that will form pair with single 5
+    cout<<count;
+
+    return 0;
+}
