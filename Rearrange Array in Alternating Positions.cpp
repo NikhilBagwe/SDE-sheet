034105110290410be84fa85313d9479e0acec0be
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-09-12 23:18:49 +0530

    Update Rearrange Array in Alternating Positions.cpp

--- Rearrange Array in Alternating Positions.cpp
+++ Rearrange Array in Alternating Positions.cpp
@@ -1,26 +1,36 @@
+// BRUTE ======================================= The order is not maintained for other test cases =========== SC=O(n) ===========================================
+
 int main()
 {
-    int a[] = {1, 2, 3, -4, -1, 4};
-    int n = 6;
+    int arr[] = {1, 2, 3, -4, -1, 4};
+    int n = 6, mid=0;
     
-    sort(a, a+n); // -4 -1 1 2 3 4 
-    vector<int> vec;
-    int i=0, j=0;
+    sort(arr, arr+n); // [-4, -1, 1, 2, 3, 4]
     
-    for(i=0; i<n; i++){
-        if(a[i]<0) vec.push_back(a[i]);
-        for(j=i+1; j<n; j++){
-            if(a[j] > 0 ){
-                vec.push_back(a[j]);
-                //i+=i;
-                break;
-            }
+    for(int i=0; i<n; i++){
+        if(arr[i] > 0){
+            mid = i;
+            break;
         }
     }
     
-    for(auto it: vec){
-        cout<<it<<" ";
+    vector<int> temp;
+    
+    int i=0, j=mid;
+    while(i<mid && j<n){
+        temp.push_back(arr[i]);
+        temp.push_back(arr[j]);
+        i++, j++;
     }
     
+    while(j<n){
+        temp.push_back(arr[j]);
+        j++;
+    }
+    
+    for(auto it: temp){
+        cout<<it<<" ";
+    }
+
     return 0;
 }
