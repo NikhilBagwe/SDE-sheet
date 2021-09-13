c2adce6b8d09f576f0fb0e0c6942fe2cdd42692f
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-09-13 13:15:08 +0530

    Update Subarray with 0 sum.cpp

--- Subarray with 0 sum.cpp
+++ Subarray with 0 sum.cpp
@@ -27,3 +27,47 @@ int main()
 
     return 0;
 }
+
+// OPTIMAL =================================================== O(n) ============== HASHMAP ===============================================================
+
+/*
+LOGIC:
+eg: a[]={4,2,-3,1,6}
+1. At i=0, sum=4 [ WE ARE DOING PREFIX SUM]
+2. at i=1, sum=6 
+3. At i=2, sum=3 
+4. At i=3, sum=4 -> map=[4 6 3 4]
+   So now the same number is getting repeated. It means after ind=0 there were some elemnets that on adding to 
+   sum, made the sum 0,and also gave the same number 4 on further iterations. So the if(map[sum]) returns true, meaning there is a subarray
+   with sum=0.
+*/
+
+int main()
+{
+    int a[]={4,2,-3,1,6};
+    int n=5;
+    int sum=0, flag=1;
+    
+    // Store the result of prefix sum
+    unordered_map<int, int> mp;
+    
+    for(int i=0; i<n; i++){
+        sum += a[i];
+        
+        // mp[sum] -> means the the current sum value is already present in the map 
+        if(sum==0 || a[i]==0 || mp[sum]){
+            flag = 1;
+            break;
+        }
+        
+        // increment the prefix sum position in map
+        else{
+            mp[sum]++;
+        }
+    }
+    
+    if(flag == 1) cout<<"Yes";
+    else cout<<"No";
+    
+    return 0;
+}
