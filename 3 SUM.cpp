f5024f4a2aa0bcf5337d579fd7dbe138457f4525
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-09-14 23:11:59 +0530

    1

--- 3 SUM.cpp
+++ 3 SUM.cpp
@@ -1,5 +1,5 @@
 // Triplet Sum in Array
-// BRUTE ======================================== O(n^2) ==============================================================================================
+// OPTIMAL  ======================================== O(n^2) ==============================================================================================
 
 int main()
 {
@@ -36,5 +36,37 @@ int main()
     
     if(flag == 0) cout<<"No";
     
+    return 0;
+}
+
+// OPTIMAL - 2 ===================================================== TC = O(n) , SC = O(n)  =========================================================================
+
+int main()
+{
+    int a[] = {1, 4, 45, 6, 10, 8};
+    int n = 6, x = 13;
+    
+    set<int> hashSet;
+    // Stores the value that needs to be found from 1st to n elements as the 0th element is fixed
+    int val = x - a[0];  // eg. val = 13-1 = 12
+    int flag = 0;
+    
+    for(int i = 1; i < n; i++){
+        // now find whether the (val-a[i]) i.e num is present in hashSet. If num is present means we can form the required sum i.e 'x' using 3 values which are-> a[0], current a[i] and num.
+        int num = val - a[i];  // eg.i=0 -> num = 12-4 = 8 -> Therefore look whether '8' is present in hashSet
+        if(hashSet.count(num)){
+            cout<<"Yes";
+            flag = 1;
+            break;
+        }
+        
+        // If num is not present, push the current element into hashSet
+        else{
+            hashSet.insert(a[i]);
+        }
+    }
+    
+    if(flag == 0) cout<<"No";
+
     return 0;
 }
