33370e5e9d3322199a09539fc8f97f13ff0a2054
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-09-14 16:42:43 +0530

    1

--- Find elements appear more than n div k.cpp
+++ Find elements appear more than n div k.cpp
@@ -1,31 +1,68 @@
-// BRUTE ======================================================================= TC = O(n) , SC = O(n) ===================================================================
+// BRUTE   ============================================================== TC = O(nlogn) ===============================================================================
+
+int main()
+{
+    int a[] = {3, 1, 2, 2, 1, 2, 3, 3};
+    int n = 8, k=4;
+    
+    sort(a, a+n);  // 1 1 2 2 2 3 3 3
+    int barrier = n/k;
+    int freq = 0;
+    int count = 0;
+    
+    for(int i=0; i<n; i++){
+        if(a[i] == a[i+1]){
+            freq += 2;
+        }
+        
+        // make freq '0' or else result will be wrong
+        if(a[i] != a[i+1]){
+            freq = 0;
+        }
+        
+        //cout<<i<<" "<<count<<" "<<freq<<endl;
+        
+        if(freq > barrier){
+            count++;
+            freq = 0;
+        }
+    }
+    
+    cout<<count;
+    
+    return 0;
+}
+
+// OPTIMAL  ======================================================================= TC = O(n) , SC = O(n) ===================================================================
+
 // link = https://www.geeksforgeeks.org/given-an-array-of-of-size-n-finds-all-the-elements-that-appear-more-than-nk-times/
+
 int main()
 {
     int a[] = {3, 1, 2, 2, 1, 2, 3, 3};
     int n = 8, k=4;
     
     map<int, int> mp;
-    int count = n/k;
+    int barrier = n/k;
     
     // store the frequency of each element in the map 
     for(int i=0; i<n; i++){
         mp[a[i]]++;
     }
     
-    int num = 0;
+    int count = 0;
     
     cout<<"Elements that appear more than n/k: ";
     for(int i=0; i<mp.size(); i++){
-        if(mp[i] > count){
-            num++;
+        if(mp[i] > barrier){
+            count++;
             // Prints the element that appears more then n/k times
             cout<<i<<" ";
         }
     }
     
     cout<<endl;
-    cout<<"Total : "<<num;
+    cout<<"Total : "<<count;
     
     return 0;
 }
