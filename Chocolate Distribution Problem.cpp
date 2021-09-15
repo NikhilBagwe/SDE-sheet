ff37ba57ca9980673f9282053e5e37c4e224b24b
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-09-15 23:41:54 +0530

    1

---
+++ Chocolate Distribution Problem.cpp
@@ -0,0 +1,25 @@
+int main()
+{
+    int a[] = {3, 4, 1, 9, 56, 7, 9, 12}; 
+    int n = 8, students = 5;
+    // int a[] = {7, 3, 2, 4, 9, 12, 56};
+    // int n = 7, students = 3;
+    
+    sort(a, a+n);  // [1, 3, 4, 7, 9, 9, 12, 56]
+    //                 i           j
+    
+    // points to the minimum and maximum values after sorting
+    int i=0, j=students-1;
+    int minDiff=INT_MAX;
+    
+    for(int i=0; i+students<n; i++){
+        if((a[j]-a[i]) < minDiff){
+            minDiff = a[j]-a[i];
+        }
+        j++;
+    }
+    
+    cout<<minDiff;
+
+    return 0;
+}
