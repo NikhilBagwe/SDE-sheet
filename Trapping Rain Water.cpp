e7a7d1f7c10c79b1b1b95e9ff188e303b0e85fa7
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-09-15 19:49:10 +0530

    1

--- Trapping Rain Water.cpp
+++ Trapping Rain Water.cpp
@@ -27,3 +27,40 @@ int main()
 
     return 0;
 }
+
+// BETTER  =============================================================== TC = O(n) , SC = O(n)  ====================================================================
+
+int main()
+{
+    int a[] = {3, 0, 2, 0, 4};  // waterTrapped = 7
+    int n = 5, waterTrapped=0;
+    
+    int maxLeft = 0, maxRight = 0;
+    
+    // Stores the maximum height from left to right
+    int prefixArr[n] = {0};   // [3 3 3 3 4]
+    
+    // Stores the maximum height from right to left 
+    int suffixArr[n] = {0};   // [4 4 4 4 4]
+    
+    // Store the 1st element a[0] into prefixArr[0] for comparison purpose
+    prefixArr[0] = a[0];
+    for(int i=1; i<n; i++){
+        prefixArr[i] = max(prefixArr[i-1], a[i]);
+    }
+    
+    // Store the Last element a[n-1] into prefixArr[n-1] for comparison purpose
+    suffixArr[n-1] = a[n-1];
+    // Start traversing from 2nd last element of a[] as we are considering the last element i.e 'i+1' for comparison
+    for(int i=n-2; i>=0; i--){
+        suffixArr[i] = max(suffixArr[i+1], a[i]);
+    }
+    
+    for(int i=0; i<n; i++){
+        waterTrapped += min(prefixArr[i], suffixArr[i]) - a[i];
+    }
+    
+    cout<<waterTrapped;
+
+    return 0;
+}
