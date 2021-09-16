93ef0572d7c951a03e7d6942897a7aa6927a1720
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-09-16 23:29:50 +0530

    1

--- Smallest subarray with sum greater than x.cpp
+++ Smallest subarray with sum greater than x.cpp
@@ -33,3 +33,53 @@ int main()
 
     return 0;
 }
+
+// OPTIMAL ======================================================= TC= O(n) SLIDING WINDOW ==============================================================================
+
+int main()
+{
+    int a[] = {1, 4, 45, 6, 0, 19};
+    int n = 6, x=51;
+    
+    // Sliding Window technique
+    
+    // Initialize the window -> starts from the 1st element and ends on 2nd element
+    int start=0, end=1;
+    int ans = INT_MAX;
+    
+    // Store 1st element to sum
+    int sum = a[start];
+    
+    // If the 1st element is greater than x than means size of minimum subarray=1 and exit the pgm.
+    if(sum > x){
+        cout<<1;
+        return 0;
+    }
+    
+    // if 'end' pointer is not exceeding size of given array 
+    if(end < n) sum += a[end];
+    
+    // keep sliding the window until the window is inside the array
+    while(start<n && end<n){
+        
+        // When (sum > x), try decreasing the size of window as we have to find the minimum subarray
+        if(sum > x){
+            ans = min(ans, (end-start+1));
+            // Decrease size of window from backwards
+            sum -= a[start];
+            start++;
+        }
+        
+        // If the desired sum is not acheived, increase the size of window from front by increasing end pointer
+        else{
+            end++;
+            if(end < n){
+                sum += a[end];
+            }
+        }
+    }
+    
+    cout<<ans;
+
+    return 0;
+}
