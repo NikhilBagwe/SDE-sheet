b8f77a4c17bcce9e407cfe8a1c4c38bb5b15988c
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-09-18 10:47:42 +0530

    Update Minimum Swaps and K together.cpp

--- Minimum Swaps and K together.cpp
+++ Minimum Swaps and K together.cpp
@@ -6,8 +6,7 @@
 ALGO:
 1. Iterate over the array and find favourable elements i.e (a[i] <= k).
 2. Create a window of size = no. favourable elements.
-3. Iterate the array and check the no. of elements in every possible window that need to be swapped in order to
-   meet the condition i.e find nonFavourable elements.
+3. Iterate the array and check the no. of elements in every possible window that need to be swapped in order to meet the condition i.e find minimum nonFavourable elements.
 */
 
 int main()
@@ -15,7 +14,7 @@ int main()
     int a[] = {2, 1, 5, 6, 3};
     int n=5, k=3;
     // int a[] = {2, 7, 9, 5, 8, 7, 4};
-    // int n=7, k=7;
+    // int n=7, k=6;
     
     // elements that are smaller or equal to 'k'
     int favourable = 0;
@@ -45,5 +44,44 @@ int main()
     }
     
     cout<<minSwap;
+    return 0;
+}
+
+// OPTIMAL ========================================================================================================================================================
+
+int main()
+{
+    int a[] = {2, 1, 5, 6, 3};
+    int n=5, k=3;
+    
+    int fav=0, nonFav=0;
+    
+    // find the favourable elements that satisfy (a[i] <= k) condition throughout the array 
+    for(int i=0; i<n; i++){
+        if(a[i] <= k) fav++;
+    }
+    
+    // Consider the 'window size=no.of favourable elements' and thus find the nonFav elements present in the window
+    // nonFav = no. of elements that needs to be swapped
+    for(int i=0; i<fav; i++){
+        if(a[i] > k) nonFav++;
+    }
+    
+    // Optimized Sliding window
+    int l=0, r=fav-1, result=INT_MAX;
+   
+    while(r < n){
+        result = min(result, nonFav);
+        // increase the window size from front side
+        r++;
+        // on increasing the size, if the new element included is greater than 'k' than increment nonFav 
+        if(r<n && a[r]>k) nonFav++;
+        // As we increase from front side, we also decrease from back side of window i.e l++. 
+        // check whether the element pointed by 'l' is greater than k, and if so decrement 'nonFav' as we are moving the window forwards
+        if(l<n && a[l]>k) nonFav--;
+        l++;
+    }
+
+    cout<<result;
     return 0;
 }
