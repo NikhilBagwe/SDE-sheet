b83d0a8e65f9c1dd59b01f3dea9e7c3103fb8c1e
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-09-21 00:12:54 +0530

    1

--- Matrix/Search in sorted 2D-MATRIX.cpp
+++ Matrix/Search in sorted 2D-MATRIX.cpp
@@ -24,3 +24,42 @@ int main()
 
     return 0;
 }
+
+// OPTIMAL  =============================================================== TC = O(n+logm) =============================================================================
+
+int main()
+{
+    int r = 3, c = 3;
+    vector<vector<int>> matrix {{1, 3, 5, 7},
+                                {10, 11, 16, 20},
+                                {23, 30, 34, 50}};
+    
+    int flag = 0, target = 23;
+    
+    // Iterate over each row and apply binary search on them
+    for(int i=0; i<r; i++){
+        // binary search
+        int start=0, end=r-1, mid=0;
+        while(start <= end){
+            mid = (start+end)/2;
+            
+            if(matrix[i][mid] == target){
+                cout<<"Element present at index : "<<i<<" "<<mid;
+                flag = 1;
+                break;
+            }
+            
+            else if(matrix[i][mid] > target){
+                end = mid-1;
+            }
+            
+            else{
+                start = mid+1;
+            }
+        }
+    }
+    
+    if(flag == 0) cout<<"Element absent";
+
+    return 0;
+}
