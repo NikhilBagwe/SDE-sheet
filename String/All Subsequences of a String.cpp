3fb52a36c6ef32c4db62372abc8c0f80d6f3447d
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-09-27 20:01:10 +0530

    1

--- String/All Subsequences of a String.cpp
+++ String/All Subsequences of a String.cpp
@@ -6,12 +6,12 @@ eg : Mic, Micro, ros, etc
 
 Subsequence : We can skip certain characters in subsequence.
 eg: st, Miro, Mit, etc
-
+  Formula = 2 ^ (length of string)
 
 */
 
 void subSequence(string input, string output, int index){
-    // Base case
+    // Base case - when we reach to end of the string, ouput the subsequence which is stored in 'output' string
     if(index >= input.size()){
         for(int i=0; i<output.size(); i++){
             cout<<output[i]<<" ";
@@ -20,13 +20,20 @@ void subSequence(string input, string output, int index){
         return;
     }
     
-    // including element
+    // Excluding element - remove the last element of output string
+    subSequence(input, output, index+1);
+    //           (abc,     "",       1)
+    //           (abc,     "",       2) -> goes to line 30 and adds "c" to output
+    //           (abc,     "",       3) -> prints empty space "" ,since o/p is empty and (index == input.size()) and returns to prev call
+    
+    // including element - add the element to the output and send it into recursion
     output += input[index];
     subSequence(input, output, index+1);
+    //           (abc,    "c",       3) -> prints "c" as (index == input.size())
     
-    // Excluding element - remove the last element of output string
-    output = output.substr(0, output.size()-1);
-    subSequence(input, output, index+1);
+    // Excluding element - Here the output will be printed upside down otherwise line 23,24 are same as line 31,32
+    //output = output.substr(0, output.size()-1);
+    //subSequence(input, output, index+1);
 }
 
 int main()
