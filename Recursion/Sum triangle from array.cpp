a1dfc5c545b5e3d54a8bd8d183b221aeded3314a
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-09-29 21:44:30 +0530

    1

--- Recursion/Sum triangle from array.cpp
+++ Recursion/Sum triangle from array.cpp
@@ -1,10 +1,13 @@
 void printTri(vector<int> &arr, int n){
+    // Base condition
     if(n<1){
         return;
     }
     
+    // Create an array and store the sum sum of consecutive elements
     vector<int> temp;
     
+    // Storing the sum of consecutive elements into the array
     for(int i=0; i<n-1; i++){
         int x = arr[i] + arr[i+1];
         temp.push_back(x);
@@ -12,6 +15,8 @@ void printTri(vector<int> &arr, int n){
     
     printTri(temp, n-1);
     
+    // We are using backtracking here to print in reverse order i.e the last array with smaller size is printed first
+    // If we cut and paste this for loop before line 17, the array with largest size is printed first.
     for(int i=0; i<n; i++){
         if(i == n-1){
             cout<<arr[i]<<" ";
@@ -20,7 +25,6 @@ void printTri(vector<int> &arr, int n){
             cout<<arr[i]<<" ";
         }
     }
-  
     cout<<endl;
 }
 
