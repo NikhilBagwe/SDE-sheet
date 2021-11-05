d904395fb59c660a7c45f42912f1192a52f8727a
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-11-05 12:20:32 +0530

    1

--- 4 Sum.cpp
+++ 4 Sum.cpp
@@ -55,3 +55,74 @@ int main()
 
     return 0;
 }
+
+
+// OPTIMAL ============================================= TC = O(N^3) =========================================================================================
+
+/*
+LOGIC:
+
+1. Sort the array
+2. Init 2 pointers.
+3. Apply TWO SUM on remaining elements i.e from 'j+1' to 'n-1' to find 3rd and 4th number.
+4. Here we are skipping the duplicates
+*/
+
+int main()
+{
+    vector<int> num {4,3,3,4,4,2,1,2,1,1};
+    int n = num.size(), target=9;
+    
+    sort(num.begin(), num.end());
+    
+    vector<vector<int>> ans; // store unique pairs
+    
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            int target_2 = target - num[i] - num[j];
+            
+            int s = j+1, e = n-1;
+            
+            // Applying TWO SUM algo -------------------------------------------
+            while(s < e){
+                int two_sum = num[s] + num[e];
+                
+                if(two_sum < target_2) s++;
+                
+                else if(two_sum > target_2) e--;
+                
+                else{
+                    vector<int> quadruplet(4, 0);
+                    quadruplet[0] = num[i];
+                    quadruplet[1] = num[j];
+                    quadruplet[2] = num[s];
+                    quadruplet[3] = num[e];
+                    
+                    ans.push_back(quadruplet);
+                    
+                    // Processing the duplicates of number 3 
+                    while(s<e && num[s]==quadruplet[2]) s++;
+                    
+                    // Processing the duplicates of number 4
+                    while(s<e && num[e]==quadruplet[3]) e--;
+                }
+            }
+            
+            // Processing the duplicates of number 2 
+            // NOTE - Move to the last duplicate number only, as the for loop will move the 'j' by one again
+            // eg: ...2 2 2 3 -> Move j to the last '2' just before '3', and then the for loop will automatically move the 'j' to '3', thus skipping the duplicates
+            while(j+1 < n && num[j+1]==num[j]) ++j;
+        }
+        
+        // Processing the duplicates of number 1
+        while(i+1 < n && num[i+1]==num[i]) ++i;
+    }
+    
+    for(auto it : ans){
+        for(auto num : it){
+            cout<<num<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
