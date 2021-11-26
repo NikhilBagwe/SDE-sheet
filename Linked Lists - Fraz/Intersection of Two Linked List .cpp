6cbecc8c298f18f769503869cb62c28790a84f94
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-11-26 13:29:50 +0530

    1

--- Linked Lists - Fraz/Intersection of Two Linked List .cpp
+++ Linked Lists - Fraz/Intersection of Two Linked List .cpp
@@ -46,3 +46,34 @@ public:
         return ptr1;
     }
 };
+
+// OPTIMAL ======================================================================================================================================================
+
+/*
+- Here we make pointers 'a' and 'b' traverse both the LL so after traversing once, next time they will stand at same intersection point.
+- Suppose we make ptr 'a' traverse LL1 and ptr 'b' traverse LL2, when they reach the last node i.e NULL pf respective LLs, make 'a' traverse LL2 and 'b' traverse LL1.
+
+- Eg: distance from head of LL1 to intersection point = m , distance from intersection point to last node = x
+      distance from head of LL2 to intersection point = n , distance from intersection point to last node = x
+      -> Thus length of LL1 = m + x 
+              length of LL2 = n + x
+      -> thus 'a' will travel a distance of 'a' = m + x + (n+x) , b = n + x + (m+x) = as we make each pointer travel both LLs.
+*/
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+        ListNode *a = headA, *b = headB;
+        
+        while(a != b){
+            if(a == NULL) a = headB;
+            else a = a->next;
+            
+            if(b == NULL) b = headA;
+            else b = b->next;
+        }
+        
+        return a;
+    }
+};
