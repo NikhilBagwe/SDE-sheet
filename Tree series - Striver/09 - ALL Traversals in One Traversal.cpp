c6df387bf28dff0d75610eaa85aa8dbfe396d97b
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-11-30 13:02:41 +0530

    1

--- Tree series - Striver/09 - ALL Traversals in One Traversal.cpp
+++ Tree series - Striver/09 - ALL Traversals in One Traversal.cpp
@@ -3,13 +3,13 @@
 - Push the root node and num=1 into it as initial configuration.
 
 - Take the topmost element from the stack and store into a variable say 'it'.
-  1. If the number stored in pair 'it' i.e it,second is '1', then push the value of it's node into "Pre-order traversal" storing vector also push the node back into stack 
+  1. If the number stored in pair 'it' i.e it.second is '1', then push the value of it's node into "Pre-order traversal" storing vector also push the node back into stack 
      by incrementing it's value by 1 and if it has a left child, then push it into the stack.
      
-  2. If the number stored in pair 'it' i.e it,second is '2', then push the value of it's node into "In-order traversal" storing vector also push the node back into stack 
+  2. If the number stored in pair 'it' i.e it.second is '2', then push the value of it's node into "In-order traversal" storing vector also push the node back into stack 
      by incrementing it's value by 1 and if it has a right child, then push it into the stack. 
      
-  3. If the number stored in pair 'it' i.e it,second is '3', then push the value of it's node into "Post-order traversal" storing vector and don't push anything.
+  3. If the number stored in pair 'it' i.e it.second is '3', then push the value of it's node into "Post-order traversal" storing vector and don't push anything.
 */
 
 class Solution {
