50d44970a6f04df83e2e536881d4c6a68116dd39
NikhilBagwe <67143015+NikhilBagwe@users.noreply.github.com>
2021-11-30 12:31:05 +0530

    1

--- Tree series - Striver/08 - Iterative Postorder Traversal using 2 and 1 Stack .cpp
+++ Tree series - Striver/08 - Iterative Postorder Traversal using 2 and 1 Stack .cpp
@@ -38,3 +38,57 @@ public:
         return ans;
     }
 };
+
+// ========================== USING 2 STACKS ================================ TC = O(2n) , SC = O(n) -- as we are using 1 stack ======================================
+
+// TC = O(2n) --- as in the worst case there will be skewed tree having only right nodes. So you end up pushing all of them into stack taking O(n) time and then again
+// traversing back using while(!st.empty() && temp == st.top()->right) loop.
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(root == NULL) return ans;
+        
+        // Init a stack 
+        stack<TreeNode*> st;
+        TreeNode *cur = root;
+        TreeNode *temp;
+        
+        while(cur != NULL || !st.empty()){
+            // Push the 'cur' node into stack and keep going towards the left of tree until you hit NULL
+            if(cur != NULL){
+                st.push(cur);
+                cur = cur->left;
+            }
+            
+            else{
+                // Once left becomes NULL, traverse towards 'right'
+                temp = st.top()->right;
+                
+                // If right is also NULL, means you are standing on a leaf node (which is a 'root' node) since 'left' is already NULL as you entered this conditional statement
+                if(temp == NULL){
+                    // Push the current node's value into 'ans'
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    
+                    // Since you popped the current node from stack, check whether the right child of the current topmost node present in stack is equal to temp or not 
+                    // i.e (temp == st.top()->right) . Here you are traversing backwards from leaf node to upwards until conditions are met
+                    while(!st.empty() && temp == st.top()->right){
+                        // Push the current topmost node's value into ans and pop it
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+                
+                else{
+                    cur = temp;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
